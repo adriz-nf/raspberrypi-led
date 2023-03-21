7a3df95f4594f040eb615d647685f711a15f4465
Andrew Emmett <71984846+adriz-nf@users.noreply.github.com>
2023-03-21 22:33:02 +0100

    Update led.py

--- led.py
+++ led.py
@@ -1,18 +1,23 @@
 # Source: https://thepihut.com/blogs/raspberry-pi-tutorials/27968772-turning-on-an-led-with-your-raspberry-pis-gpio-pins
-def flash(blinks=5, ms=500):
+def flash(blinks=5, ms=500, pin=18):
   import RPi.GPIO as GPIO
   import time
+  blinks = int(blinks)
+  ms = int(ms)/1000
+  pin = int(pin)
   GPIO.setmode(GPIO.BCM)
   GPIO.setwarnings(False)
   for i in range(1,blinks+1):
-    GPIO.setup(18,GPIO.OUT)
+    GPIO.setup(pin,GPIO.OUT)
     print(f"LED on. {i} of {blinks}")
-    GPIO.output(18,GPIO.HIGH)
-    time.sleep(ms/1000)
-    GPIO.output(18,GPIO.LOW)
+    GPIO.output(pin,GPIO.HIGH)
+    time.sleep(ms)
+    GPIO.output(pin,GPIO.LOW)
     print("LED off")
-    time.sleep(ms/1000)
+    time.sleep(ms)
 
+  
+  
 if __name__ == '__main__':
     import sys
-    flash(int(sys.argv[1]), int(sys.argv[2]))
+    flash(*sys.argv[1:])
